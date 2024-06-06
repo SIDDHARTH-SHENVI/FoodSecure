ebd0bb5b764bd272c06d7683facc33da2f4d0145
SIDDHARTH-SHENVI <siddharthshenvi49@gmail.com>
2024-06-07 02:48:37 +0530

    first commit

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbarr from './components/Navbarr';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (<>
+   <Navbarr/>
+   <Banner/>
+   <Footer/>
+</>)
+}
+
+export default App;
---
+++ frontend/frontend/src/components/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import banner from '../../public/banner.png'
+function Banner() {
+  return (<>
+    <div className='max-w-screen-2x1 container mx-auto md:px px-4 flex flex-col md:flex-row'>
+      <div className='w-full md:w-1/2 mt-12 md:mt-32'>
+        <div className='space-y-12'>
+        <h1 className='text-4xl font-bold'>End Hunger! Reduce Waste! Empower Communities!</h1>
+        <p className='text-xl '>FoodSecure connects surplus food from suppliers directly to those in need, ensuring that no food goes to waste. By joining us, you can help combat hunger, support sustainable practices, and strengthen community bonds. Together, we can create a healthier, more resilient world</p>
+
+        
+        <label className="input input-bordered flex items-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+  <input type="text" className="grow" placeholder="Email" />
+</label>
+</div>
+<button className="btn mt-6 btn-secondary">Get Started</button>
+      </div>
+
+      <div className='w-full md:w-1/2'>
+        <img src={banner} className='w-92 h-92' alt=''/>
+      </div>
+    </div>
+    </>)
+}
+
+export default Banner
